065cd65be57b7e087a4628b89136a3e9635e7610
ndeereedwin <ndeereedwin@gmail.com>
2021-10-13 11:44:12 +0300

    main

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .webpackConfig(require('./webpack.config'));
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
 
 if (mix.inProduction()) {
     mix.version();
