65101c979acc6380d1b06c0485fa3c7b85df8963
ndeereedwin <ndeereedwin@users.noreply.github.com>
2022-07-15 12:51:13 +0300

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,16 +9,17 @@ Alpine.start();
 // Import admin-lte-3
 import 'admin-lte';
 import 'admin-lte/plugins/jquery/jquery';
+import '../sass/app.scss';
 
-// nav-item 
+// nav-item
 var url = window.location;
 
 const allLinks = document.querySelectorAll('.nav-item a');
 const currentLink = [...allLinks].filter(e => {
     return e.href == url;
 });
-    
-if (currentLink.length > 0) { 
+
+if (currentLink.length > 0) {
     currentLink[0].classList.add("active");
     currentLink[0].closest(".nav-treeview").style.display = "block";
     currentLink[0].closest(".has-treeview").classList.add("active");
